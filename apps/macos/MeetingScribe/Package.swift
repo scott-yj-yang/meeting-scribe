c3552b77e746e388aee27c1c85ebc2946537ebcd
Scott Yang <67733409+scott-yj-yang@users.noreply.github.com>
2026-03-25 21:29:15 -0700

    fix: lower swift-tools-version to 6.0 for compatibility with Xcode 16

--- apps/macos/MeetingScribe/Package.swift
+++ apps/macos/MeetingScribe/Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 6.0
 import PackageDescription
 
 let package = Package(
     name: "MeetingScribe",
-    platforms: [.macOS(.v26)],
+    platforms: [.macOS("26.0")],
     dependencies: [
         // TODO: Add WhisperKit dependency once it supports macOS 26 SDK
         // .package(url: "https://github.com/argmaxinc/WhisperKit.git", from: "0.9.0"),
